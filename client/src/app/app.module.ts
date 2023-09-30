43f99b2afbf928ede53526925e27c33fabe90ac6
M-a-a-d-man <79801553+M-a-a-d-man@users.noreply.github.com>
2023-09-30 18:26:57 -0400

    Staged changes for clean merge

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JobModalComponent } from './Components/job-modal/job-modal.component';
+import {MatCardModule} from '@angular/material/card';
 import { CardComponent } from './Components/card/card.component';
 import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { MatButtonModule } from '@angular/material/button';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
