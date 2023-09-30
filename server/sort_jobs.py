b6689147e3772fa4f1f24da92ac3eb70df76438d
Jordan Marcus <mrjordanmarcus@gmail.com>
2023-09-30 17:58:06 -0400

    Update

--- server/sort_jobs.py
+++ server/sort_jobs.py
@@ -1,33 +1,39 @@
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
+from transformers import pipeline
 
-model = SentenceTransformer('multi-qa-mpnet-base-dot-v1')
+similarityModel = SentenceTransformer('multi-qa-mpnet-base-dot-v1')
+sentimentAnalysisModel = pipeline('sentiment-analysis', 'distilbert-base-uncased-finetuned-sst-2-english')
 
 jobs = ["We are looking for a professional banana eater with decades of experience."]
 
 resumes = ["I have many skills, such as eating 2 bananas at once.",
-           "I've eaten bananas before, I guess, but I'm not too confident.",
+           "I've eaten bananas before, I guess.",
            "Banana-eating world champion 2013-2023. One decade, baby.",
-           "ligma balls"]
+           "ehhh."]
 
-jobEmbeddings = model.encode(jobs)
-resumeEmbeddings = model.encode(resumes)
+jobEmbeddings = similarityModel.encode(jobs)
+resumeEmbeddings = similarityModel.encode(resumes)
 
 for job, jobEmbedding in zip(jobs, jobEmbeddings):
     jobEmbedding = jobEmbedding.reshape(1, -1)
-    print("Job:", job)
-    print("")
+    print('Job:', job)
+    print('')
     for resume, resumeEmbedding in zip(resumes, resumeEmbeddings):
         resumeEmbedding = resumeEmbedding.reshape(1, -1)
         similarity = cosine_similarity(resumeEmbedding, jobEmbedding)[0][0]
-        print("Resume:", resume)
-        print("Similarity:", similarity)
-        print("")
+        print('Resume:', resume)
+        print('Similarity:', similarity)
+        print('')
 
+# Find the top-10 resumes predicted by the KNN algorithm using Cosine similarity value
+
+
+
+
+# Sentiment analysis
 '''
-embedding1 = model.encode(sentence1).reshape(1, -1)
-embedding2 = model.encode(sentence2).reshape(1, -1)
-
-similarity = cosine_similarity(embedding1, embedding2)[0][0]
-print(similarity)
+for resume in resumes:
+    sentimentResult = sentimentAnalysisModel(resume)
+    print(sentimentResult[0]['label'], sentimentResult[0]['score'])
 '''
