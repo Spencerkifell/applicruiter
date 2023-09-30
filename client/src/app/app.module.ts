049b64ad5f2b952202288d1268833f755723a619
M-a-a-d-man <79801553+M-a-a-d-man@users.noreply.github.com>
2023-09-30 15:23:25 -0400

    add update app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Pages/Home/home/home.component';
+import { CardComponent } from './Components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
