0db7b71c407c8ca1b2f7849b149c41122c4b2647
Spencer Kifell <spencerkfl@gmail.com>
2023-09-30 14:49:24 -0400

    Fixed incorrect file structure

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './Pages/Home/home/home.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Pages/Home/home/home.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 @NgModule({
   declarations: [
