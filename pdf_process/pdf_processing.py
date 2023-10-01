740c121692e186e495bfc65de029016204ebca12
Joshua-E-Weinstein <Joshua.E.Weinstein@gmail.com>
2023-09-30 20:06:56 -0400

    Resumes

--- pdf_process/pdf_processing.py
+++ pdf_process/pdf_processing.py
@@ -5,15 +5,19 @@ from pdf_process.resume import *
 
 pdfs_folder = "D:\\AI_Projects\\Mais2023\\archive\\data\\data\\DESIGNER"
 
+resumes = dict()
+
 if __name__ == '__main__':
     pdf_files = [f for f in os.listdir(pdfs_folder) if f.endswith(".pdf")]
-    # print(pdf_files)
 
+    # Parse each resume.
     for pdf_file in pdf_files:
-        pdf_file_path = os.path.join(pdfs_folder, pdf_file)
+        pdf_file_path = os.path.join(pdfs_folder, pdf_file) # Get full filepath.
 
-        # Create a Resume object for each PDF
+        # Create a Resume object for each PDF and add to dictionary.
         resume = Resume(pdf_file, pdf_file_path)
 
-        # Print or process the content as needed
-        print("===========================", resume.get_pdf_content())
+        resumes[resume.get_candidate_name()] = resume
+
+        # Print or process the content as needed.
+
--- server/sort_jobs.py
+++ server/sort_jobs.py
@@ -5,17 +5,16 @@ from transformers import pipeline
 similarityModel = SentenceTransformer('multi-qa-mpnet-base-dot-v1')
 sentimentAnalysisModel = pipeline('sentiment-analysis', 'distilbert-base-uncased-finetuned-sst-2-english')
 
-jobs = ["We are looking for a frontend programmer."]
+jobs1 = ["We are looking for a frontend programmer."]
 
-resumes = ["I am a programmer who can do frontend.",
+resumes1 = ["I am a programmer who can do frontend.",
            "Banana-eating world champion 2013-2023. One decade, baby.",
            "I am a programmer who can do backend.",
            "I am a programmer who can do frontend and backend.",
            "I have done an intro to Python class!",
            "I am a programmer who can do frontend and backend with 20 years of experience."]
 
-jobEmbeddings = similarityModel.encode(jobs)
-resumeEmbeddings = similarityModel.encode(resumes)
+
 
 '''
 # Sentiment analysis
@@ -26,29 +25,33 @@ for resume in resumes:
 
 maxResumeResults = 10;
 
-for job, jobEmbedding in zip(jobs, jobEmbeddings):
-    jobEmbedding = jobEmbedding.reshape(1, -1)
-    print('Job:', job)
-    print('')
-    resumeSimilarityList = []
-
-    for resume, resumeEmbedding in zip(resumes, resumeEmbeddings):
-        resumeEmbedding = resumeEmbedding.reshape(1, -1)
-        similarity = cosine_similarity(resumeEmbedding, jobEmbedding)[0][0]
-        resumeSimilarityList.append((resume, similarity))
-
-    resumeSimilarityList.sort(key=lambda x: x[1], reverse=True)
-
-    # Implement KNN to get the top maxResumeResults resumes.
-
-    for i in range(min(maxResumeResults, len(resumeSimilarityList))):
-        resumeSimilarity = resumeSimilarityList[i]
-        print(i + 1, ":", sep="")
-        print('Resume:', resumeSimilarity[0])
-        print('Similarity:', resumeSimilarity[1])
+def rank_jobs(jobs, resumes):
+    jobEmbeddings = similarityModel.encode(jobs)
+    resumeEmbeddings = similarityModel.encode(resumes)
+    for job, jobEmbedding in zip(jobs, jobEmbeddings):
+        jobEmbedding = jobEmbedding.reshape(1, -1)
+        print('Job:', job)
         print('')
+        resumeSimilarityList = []
+
+        for resume, resumeEmbedding in zip(resumes, resumeEmbeddings):
+            resumeEmbedding = resumeEmbedding.reshape(1, -1)
+            similarity = cosine_similarity(resumeEmbedding, jobEmbedding)[0][0]
+            resumeSimilarityList.append((resume, similarity))
+
+        resumeSimilarityList.sort(key=lambda x: x[1], reverse=True)
+
+        # Implement KNN to get the top maxResumeResults resumes.
+
+        for i in range(min(maxResumeResults, len(resumeSimilarityList))):
+            resumeSimilarity = resumeSimilarityList[i]
+            print(i + 1, ":", sep="")
+            print('Resume:', resumeSimilarity[0])
+            print('Similarity:', resumeSimilarity[1])
+            print('')
 
 
-
+if __name__ == '__main__':
+    rank_jobs(jobs1, resumes1)
 
 
