2d0ea03c33105bff544355f107c86803e7b18e04
Spencer Kifell <spencerkfl@gmail.com>
2023-10-01 01:52:20 -0400

    Added data subscription with rxjs to close modal when post is added

--- client/src/app/Services/data/data.service.ts
+++ client/src/app/Services/data/data.service.ts
@@ -8,9 +8,16 @@ export class DataService {
   private jobList = new BehaviorSubject([]);
   sharedJobList = this.jobList.asObservable();
 
+  private jobModalStatus = new BehaviorSubject<boolean>(false);
+  sharedJobModalStatus = this.jobModalStatus.asObservable();
+
   constructor() { }
 
   updateJobList(data: any) {
     this.jobList.next(data);
+  }
+
+  modalIsCompleted(data: boolean) {
+    this.jobModalStatus.next(data);
   }
 }
