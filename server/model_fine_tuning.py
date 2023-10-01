e74d199cbaac1f05bf2946c9d4972258e855510d
Jordan Marcus <mrjordanmarcus@gmail.com>
2023-09-30 23:21:21 -0400

    Implement fine tuning

--- server/model_fine_tuning.py
+++ server/model_fine_tuning.py
@@ -1,20 +1,47 @@
+import csv
 from sentence_transformers import SentenceTransformer, InputExample, losses, evaluation
-from sentence_transformers.readers import STSDataReader
+import torch
 from torch.utils.data import DataLoader
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+else:
+    device = torch.device("cpu")
+print("Using device:", device)
+
+category_resume_dict = {}
+with open('ResumeDataSet.csv', 'r', newline='', encoding='utf-8') as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader, None)
+    for row in csv_reader:
+        category, resume = row[0], row[1]
+        if category not in category_resume_dict:
+            category_resume_dict[category] = []
+        category_resume_dict[category].append(resume)
+
+similarity_amount = 0.8
+train_examples = []
+for category in category_resume_dict:
+    for i in range(0, len(category_resume_dict[category]) - 1):
+        if len(category_resume_dict[category]) <= 1:
+            break
+        train_examples.append(InputExample(texts=[category_resume_dict[category][i], category_resume_dict[category][i + 1]], label=similarity_amount))
+        '''train_examples.append((category_resume_dict[category][i], category_resume_dict[category][i + 1], similarity_amount))
+print()
+print(train_examples)'''
+
 
 model_save_path = "model"
 
 model = SentenceTransformer('all-mpnet-base-v2')
 
-train_examples = [InputExample(texts=['My first sentence', 'My second sentence'], label=0.8),
-                  InputExample(texts=['Another pair', 'Unrelated sentence'], label=0.3)]
+'''train_examples = [InputExample(texts=['My first sentence', 'My second sentence'], label=0.8),
+                  InputExample(texts=['Another pair', 'Unrelated sentence'], label=0.3)]'''
 train_batch_size = 16
 train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=train_batch_size)
 train_loss = losses.CosineSimilarityLoss(model=model)
 
-sts_reader = STSDataReader('../data/quora', normalize_scores=True, s1_col_idx=4, s2_col_idx=5, score_col_idx=6, max_score=1)
-
-evaluator = evaluation.EmbeddingSimilarityEvaluator.from_input_examples(sts_reader.get_examples('sts-dev.csv'))
+evaluator = evaluation.EmbeddingSimilarityEvaluator.from_input_examples(train_examples, name='sts-eval')
 
 model.fit(train_objectives=[(train_dataloader, train_loss)],
           evaluator=evaluator,
